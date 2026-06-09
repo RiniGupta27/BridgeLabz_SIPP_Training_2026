84e6cf4031a5dd0e51009e623bb22479fcb4f41c
RiniGupta27 <rini.g.2005@gmail.com>
2026-06-09 14:07:34 +0530

    [Rini].ADD added level01 question for programming element

---
+++ gcr_codebase/programming_elements/Level_1/distance.java
@@ -0,0 +1,7 @@
+public class distance{
+    public static void main(String[] args) {
+        double km=10.8;
+        double miles=km*0.6;
+        System.out.println("The distance " + km +"km in miles: "+miles);
+    }
+}
