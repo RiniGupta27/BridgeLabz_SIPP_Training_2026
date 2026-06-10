32d9bb0f96067f312bb233c92dd686e7aac3df72
RiniGupta27 <rini.g.2005@gmail.com>
2026-06-10 11:01:05 +0530

    Delete gcr_codebase/programming_elements/Level_1 directory

--- gcr_codebase/programming_elements/Level_1/SamMarks.java
+++
@@ -1,9 +0,0 @@
-public class SamMarks{
-    public static void main(String[] args) {
-        int Maths=94;
-        int Physics=95;
-        int Chemistry=96;
-        int averageMarks=(Maths+Physics+Chemistry)/3;
-        System.out.println("Sam’s average mark in PCM is:"+averageMarks);
-    }
-}
